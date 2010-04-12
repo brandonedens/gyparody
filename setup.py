a42d4f1a838c26b2f99bc9a0178749a515a7eb8b
Brandon Edens <brandon@as220.org>
2010-04-12 10:40:34 -0400

    Add gyparody python egg.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='gyparody',
+      version=version,
+      description="Gyparody a game to play with friends.",
+      long_description="""\
+Gyparody is a game to play with friends and an audience.""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='gyparody game-show',
+      author='Brandon Edens',
+      author_email='brandon@as220.org',
+      url='http://as220.org/git/gyparody/',
+      license='GPL3',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
